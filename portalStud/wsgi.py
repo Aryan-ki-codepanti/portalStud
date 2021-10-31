c58fb64c664f27f105db40bbb3e9a93858a53b8e
Aryan Sethi <aryan.rdps@gmail.com>
2021-10-31 23:39:01 +0530

    Initial Commit with migrations

---
+++ portalStud/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for portalStud project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portalStud.settings')
+
+application = get_wsgi_application()
