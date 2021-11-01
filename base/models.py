394c22392034a69da2e93ee49e8c3ecd25bb1219
Aryan Sethi <aryan.rdps@gmail.com>
2021-11-01 14:44:01 +0530

    Added str to Marksheet

--- base/models.py
+++ base/models.py
@@ -13,12 +13,15 @@ class User(AbstractUser):
     REQUIRED_FIELDS = [ 'username' ]
 
 class MarkSheet(models.Model):
-    english = models.DecimalField(default=0)
-    maths = models.DecimalField(default=0)
-    physics = models.DecimalField(default=0)
-    chemistry = models.DecimalField(default=0)
-    computer_science = models.DecimalField(default=0)
+    english = models.DecimalField(default=0 , decimal_places=1 , max_digits=5)
+    maths = models.DecimalField(default=0 , decimal_places=1 , max_digits=5)
+    physics = models.DecimalField(default=0 , decimal_places=1 , max_digits=5)
+    chemistry = models.DecimalField(default=0 , decimal_places=1 , max_digits=5)
+    computer_science = models.DecimalField(default=0 , decimal_places=1 , max_digits=5)
     grade = models.CharField(max_length=1 , default='A')
 
     student = models.ForeignKey(User , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.student.username}'s Scorecard : {self.grade}"
 
