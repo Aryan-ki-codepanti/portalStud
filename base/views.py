74aa75450e6ad277bc78957441d9289531aef764
Aryan Sethi <aryan.rdps@gmail.com>
2021-11-01 22:53:19 +0530

    Added home view

--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import User , MarkSheet
 
 # Create your views here.
+
+def home(request):
+    
+    context = {
+        
+    }
+    return render(request , "base/home.html" , context)
+
