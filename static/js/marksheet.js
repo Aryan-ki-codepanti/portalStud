53bb676a8ab05da580fac61ca77ab50e12bc574d
Aryan Sethi <aryan.rdps@gmail.com>
2021-11-12 18:53:20 +0530

    In JS file , added box shadow color to add shadows to marksheets , added CSS to box marksheet elements with some mediaqueries , Also styled heading of page

--- static/js/marksheet.js
+++ static/js/marksheet.js
@@ -1,19 +1,44 @@
 const colors = [
-    "#6200EE" ,
-    "#000" ,
-    "#018786" ,
-    "#FC6E51" ,
-    "#3700B3" ,
-    "#ADD468" ,
-    "#D8334A"
+    {
+        color: "#6200EE",
+        shadowColor: "rgba(98, 0, 238, 0.44)"
+    },
+    {
+        color: "#000",
+        shadowColor: "rgba(0 , 0, 0 , 0.44)"
+    },
+    {
+        color: "#018786",
+        shadowColor: "rgba(1, 135, 135, 0.44)"
+    },
+    {
+        color: "#FC6E51",
+        shadowColor: "rgb(252, 110, 81 , 0.44)"
+    },
+    {
+        color: "#3700B3",
+        shadowColor: "rgb(55, 0, 179, 0.44)"
+    },
+    {
+        color: "#ADD468",
+        shadowColor: "rgb(173, 212, 104 , 0.44)"
+    },
+    {
+        color: "#D8334A",
+        shadowColor: "rgb(216, 51, 74 , 0.44)"
+    }
 ];
 
-const getColor = count => colors[ (count % colors.length)] ;
+const getColorSet = count => colors[count % colors.length];
 
 const markSheetContainer = document.querySelector(".marksheet-container");
-const marksheets = Array.from(markSheetContainer.getElementsByClassName("marksheet"));
+const marksheets = Array.from(
+    markSheetContainer.getElementsByClassName("marksheet")
+);
 
 for (let num = 0; num < marksheets.length; num++) {
     const marksheet = marksheets[num];
-    marksheet.style.backgroundColor = getColor(num);
+    const { color, shadowColor } = getColorSet(num);
+    marksheet.style.backgroundColor = color;
+    marksheet.style.boxShadow = `${shadowColor}  8px 8px 10px`;
 }
